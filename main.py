ff3d2eab276e3eaffb84821714c30e2f8498a756
MarcoGarf <marcogarf200@gmail.com>
2023-11-17 20:21:12 -0700

    dsf

--- main.py
+++
@@ -1,9 +0,0 @@
-from CityGenerator import CityGenerator
-
-if __name__ == "__main__":
-   
-    city_generator = CityGenerator(size=10)
-    city_generator.generate_city()  # Ensure the city is generated before displaying
-    city_generator.display_city()
-   
-    
